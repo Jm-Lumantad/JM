4e84a0d5b24066b1af50b1117711ec97e567e2c4
Jm-Lumantad <56587796+Jm-Lumantad@users.noreply.github.com>
2019-12-21 14:05:54 +0700

    Add files via upload
    Whileloop

---
+++ WhileLoop.java
@@ -0,0 +1,16 @@
+
+package lumantadjm;
+
+public class Lumantadjm {
+
+    public static void main(String[] args) {
+        int lumantad = 1;
+        
+        while(lumantad<=10){
+            System.out.println(lumantad);
+            lumantad++;
+            
+        }
+    }
+  }
+    
